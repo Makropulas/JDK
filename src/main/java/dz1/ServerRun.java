1093ffacfba78ad9a5be7a6b274dff08cfb7eac9
DmitryMalomoshin <makropulas@gmail.com>
2023-11-29 15:02:59 +0300

    Changed ServerRun

--- src/main/java/dz1/ServerRun.java
+++ src/main/java/dz1/ServerRun.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ServerRun extends JFrame {
+public class ServerRun extends JFrame implements ChatServerListener {
     private static final Dimension displaySize = Toolkit.getDefaultToolkit().getScreenSize();
     private static final int WINDOW_HEIGHT = 300;
     private static final int WINDOW_WIDTH = 450;
@@ -14,29 +14,16 @@ public class ServerRun extends JFrame {
     private final JTextArea log = new JTextArea();
     private final JButton btnStart = new JButton("Start Server");
     private final JButton btnStop = new JButton("Stop Server");
-    private boolean isServerWorking;
+    private final ChatServer server;
 
     ServerRun() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(WINDOW_POSX, WINDOW_POSY, WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Chat Server");
         setResizable(false);
-        btnStart.addActionListener(e -> {
-            if (!isServerWorking) {
-                isServerWorking = true;
-                log.append(serverStatus());
-            } else JOptionPane.showMessageDialog(this,
-                    "Команда '" + btnStart.getText() + "' невозможна, т.к. сервер уже запущен",
-                    "ВНИМАНИЕ", JOptionPane.WARNING_MESSAGE);
-        });
-        btnStop.addActionListener(e -> {
-            if (isServerWorking) {
-                isServerWorking = false;
-                log.append(serverStatus());
-            } else JOptionPane.showMessageDialog(this,
-                    "Команда '" + btnStop.getText() + "' невозможна, т.к. сервер не запущен",
-                    "ВНИМАНИЕ", JOptionPane.WARNING_MESSAGE);
-        });
+        server = new ChatServer(this);
+        btnStart.addActionListener(e -> server.start(btnStart.getText(), this));
+        btnStop.addActionListener(e -> server.stop(btnStop.getText(), this));
 
         JPanel btnPanel = new JPanel(new GridLayout(1, 2));
         btnPanel.add(btnStart);
@@ -51,13 +38,51 @@ public class ServerRun extends JFrame {
         setVisible(true);
     }
 
+    @Override
+    public void onMessageReceived(String msg) {
+        log.append(msg);
+    }
+
+    public static void main(String[] args) {
+        new ServerRun();
+    }
+
+}
+
+interface ChatServerListener {
+    void onMessageReceived(String msg);
+}
+
+class ChatServer {
+    private boolean isServerWorking;
+    private final ChatServerListener listener;
+
+    ChatServer(ChatServerListener listener) {
+        isServerWorking = false;
+        this.listener = listener;
+    }
+
     private String serverStatus() {
         return String.format("Статус сервера: %s (%s)\n",
                 isServerWorking ? "Запущен" : "Остановлен",
                 new SimpleDateFormat().format(new Date()));
     }
 
-    public static void main(String[] args) {
-        new ServerRun();
+    void start(String command, ServerRun server) {
+        if (!isServerWorking) {
+            isServerWorking = true;
+            listener.onMessageReceived(serverStatus());
+        } else JOptionPane.showMessageDialog(server,
+                "Команда '" + command + "' невозможна, т.к. сервер уже запущен",
+                "ВНИМАНИЕ", JOptionPane.WARNING_MESSAGE);
+    }
+
+    void stop(String command, ServerRun server) {
+        if (isServerWorking) {
+            isServerWorking = false;
+            listener.onMessageReceived(serverStatus());
+        } else JOptionPane.showMessageDialog(server,
+                "Команда '" + command + "' невозможна, т.к. сервер не запущен",
+                "ВНИМАНИЕ", JOptionPane.WARNING_MESSAGE);
     }
 }
